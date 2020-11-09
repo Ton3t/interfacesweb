fc1fedbed7bac4444ed3567661e715e6b188585e
Ton3t <elton3t@gmail.com>
2020-11-10 00:15:00 +0100

    mapbox y JSON

--- UD04/P01/app01.js
+++ UD04/P01/app01.js
@@ -1,116 +1,22 @@
-
-
 function map() {
+    var cord = JSON.parse(localStorage.getItem("coordenas"));
+    console.log(cord);
     mapboxgl.accessToken = 'pk.eyJ1IjoidG9uM3QiLCJhIjoiY2toMG5lMHB5MDZpbzJ3c2lvYmp3MHMxbCJ9.RchSMZrgFnCwuqSJ1BlwyQ';
     var map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [cord.longitud, cord.latitud],
+        zoom: 10,
     });
     map.addControl(new mapboxgl.NavigationControl());
     map.addControl(new mapboxgl.FullscreenControl({
         container: document.querySelector('mapa')
     }));
     
-    
-    var size = 100;
-    
-    //var localizacion = obtener();
-
-    // implementation of CustomLayerInterface to draw a pulsing dot icon on the map
-    // see https://docs.mapbox.com/mapbox-gl-js/api/#customlayerinterface for more info
-    var pulsingDot = {
-        width: size,
-        height: size,
-        data: new Uint8Array(size * size * 4),
-
-        // get rendering context for the map canvas when layer is added to the map
-        onAdd: function () {
-            var canvas = document.createElement('canvas');
-            canvas.width = this.width;
-            canvas.height = this.height;
-            this.context = canvas.getContext('2d');
-        },
-
-        // called once before every frame where the icon will be used
-        render: function () {
-            var duration = 1000;
-            var t = (performance.now() % duration) / duration;
-
-            var radius = (size / 2) * 0.3;
-            var outerRadius = (size / 2) * 0.7 * t + radius;
-            var context = this.context;
-
-            // draw outer circle
-            context.clearRect(0, 0, this.width, this.height);
-            context.beginPath();
-            context.arc(
-                this.width / 2,
-                this.height / 2,
-                outerRadius,
-                0,
-                Math.PI * 2
-            );
-            context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-            context.fill();
-
-            // draw inner circle
-            context.beginPath();
-            context.arc(
-                this.width / 2,
-                this.height / 2,
-                radius,
-                0,
-                Math.PI * 2
-            );
-            context.fillStyle = 'rgba(255, 100, 100, 1)';
-            context.strokeStyle = 'white';
-            context.lineWidth = 2 + 4 * (1 - t);
-            context.fill();
-            context.stroke();
-
-            // update this image's data with data from the canvas
-            this.data = context.getImageData(
-                0,
-                0,
-                this.width,
-                this.height
-            ).data;
-
-            // continuously repaint the map, resulting in the smooth animation of the dot
-            map.triggerRepaint();
-
-            // return `true` to let the map know that the image was updated
-            return true;
-        }
-    };
-
-    map.on('load', function () {
-        map.addImage('pulsing-dot', pulsingDot, {
-            pixelRatio: 2
-        });
-
-        map.addSource('points', {
-            'type': 'geojson',
-            'data': {
-                'type': 'FeatureCollection',
-                'features': [{
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': []
-                    }
-                }]
-            }
-        });
-        map.addLayer({
-            'id': 'points',
-            'type': 'symbol',
-            'source': 'points',
-            'layout': {
-                'icon-image': 'pulsing-dot'
-            }
-        });
-    });
+    var marker = new mapboxgl.Marker()
+        .setLngLat([cord.longitud, cord.latitud])
+        .setPopup(new mapboxgl.Popup().setHTML("<h1>Hola!!</h1><p>Según tu geolocalización estás mas o menos por esta zona...<br/>:D</p>"))
+        .addTo(map);
 }
 
 function onload() {
@@ -127,23 +33,26 @@ function obtener() {
         maximumAge: 60000
     };
     console.log(geoconfig);
-
     navigator.geolocation.getCurrentPosition(mostrar, errores, geoconfig);
+    console.log(navigator.geolocation.getCurrentPosition(mostrar, errores, geoconfig));
 }
 
 function mostrar(posicion) {
+    var datos = {
+        "latitud": posicion.coords.latitude,
+        "longitud": posicion.coords.longitude,
+        "exactitud": posicion.coords.accuracy
+    }
+
+    var objeto = JSON.stringify(datos);
+    localStorage.setItem("coordenas", objeto);
+    /*
     var datos = "";
     datos += 'Latitud: ' + posicion.coords.latitude + '\n';
     datos += 'Longitud: ' + posicion.coords.longitude + '\n';
     datos += 'Exactitud: ' + posicion.coords.accuracy + 'mts.\n';
+    */
     console.log(datos);
-}
-
-function mostrarLatitud(posicion) {
-    var datos =  posicion.coords.latitude;
-    var lat = parseFloat(datos);
-    console.log(lat);
-    return lat;
 }
 
 function errores(error) {
@@ -162,8 +71,6 @@ function errores(error) {
             break;
     }
 }
-
-
 
 function anyadeMapa() {
     var zona = document.getElementById("map");
